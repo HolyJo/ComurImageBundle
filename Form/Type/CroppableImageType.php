271f11925e6da06a0d5a499f11222fa1f4cfddc7
Nathan O'Hanlon <nathan@prospertechnologies.com>
2015-09-01 13:10:13 +0100

    Update CroppableImageType.php
    Make preview option

--- Form/Type/CroppableImageType.php
+++ Form/Type/CroppableImageType.php
@@ -12,6 +12,7 @@ use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class CroppableImageType extends AbstractType
 {
@@ -44,12 +45,12 @@ class CroppableImageType extends AbstractType
             $builder->add($options['uploadConfig']['saveOriginal'], 'text', array(
                 // 'inherit_data' => true,
                 // 'property_path' => $options['uploadConfig']['saveOriginal'],
-                'attr' => array('style' => 'opacity: 0;width: 0; max-width: 0; height: 0; max-height: 0;')));
+                'attr' => array('style' => 'opacity: 0;width: 0; max-width: 0; height: 0; max-height: 0; position: absolute;')));
         }
         $builder->add($builder->getName(), 'text', array(
             // 'property_path' => $builder->getName(),
             // 'inherit_data' => true,
-            'attr' => array('style' => 'opacity: 0;width: 0; max-width: 0; height: 0; max-height: 0;')));
+            'attr' => array('style' => 'opacity: 0;width: 0; max-width: 0; height: 0; max-height: 0; position: absolute;')));
     }
 
     /**
@@ -86,10 +87,14 @@ class CroppableImageType extends AbstractType
             //     return $options['uploadConfig']['saveOriginal'] ? true : false;
             // },
             'inherit_data' => true,
+            'help' => null,
+            'show_fade' => false
             // 'property_path' => null,
             // 'data_class' => 'MVB\Bundle\MemberBundle\Entity\Member'
         ));
-        
+
+        $resolver->setOptional(array('help', 'show_fade', 'image_checker', 'image_path'));
+
         $isGallery = $this->isGallery;
         $galleryDir = $this->galleryDir;
 
@@ -113,7 +118,7 @@ class CroppableImageType extends AbstractType
             },
             'cropConfig' => function(Options $options, $value) use($cropConfig){
                 return array_merge($cropConfig, $value);
-            },
+            }
             // 'compound' => function(Options $options, $value) use($cropConfig){
             //     return $options['uploadConfig']['saveOriginal'] ? true : false;
             // }
@@ -151,6 +156,32 @@ class CroppableImageType extends AbstractType
         }
 
         $view->vars['options'] = array('uploadConfig' => $uploadConfig, 'cropConfig' => $cropConfig, 'fieldImage' => $fieldImage);
-        $view->vars['attr'] = array('style' => 'opacity: 0;width: 0; max-width: 0; height: 0; max-height: 0;');
+        $view->vars['attr'] = array('style' => 'opacity: 0;width: 0; max-width: 0; height: 0; max-height: 0; position: absolute;');
+
+        if (array_key_exists('help', $options)) {
+            $view->vars['help'] = $options['help'];
+        }
+
+        if (array_key_exists('show_fade', $options)) {
+            $view->vars['show_fade'] = $options['show_fade'];
+        }
+
+
+        if ((array_key_exists('image_path', $options)) && (array_key_exists('image_checker', $options))) {
+            $parentData = $form->getParent()->getData();
+
+            $imageUrl = null;
+            if (null !== $parentData) {
+                $accessor = PropertyAccess::createPropertyAccessor();
+                if (true === $accessor->getValue($parentData, $options['image_checker'])) {
+                    $imageUrl = $accessor->getValue($parentData, $options['image_path']);
+                }
+            }
+
+            // set an "image_url" variable that will be available when rendering this field
+            $view->vars['image_url'] = $imageUrl;
+
+            $view->vars['image_url_faded'] = $imageUrl;
+        }
     }
 }
